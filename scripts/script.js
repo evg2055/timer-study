8a67b3b1aed0f57abd8a08b1d3231a84dcd23270
EVG2055 <enivanoff55@mail.ru>
2022-05-15 16:19:35 +0600

    cleaned up code

--- scripts/script.js
+++ scripts/script.js
@@ -44,40 +44,42 @@
 'use strict';
 const BUTTON = document.querySelector('.button');
 let intervalId2;
-const HOURDISPLAY = document.querySelector('.hour');
-const MINUTESDISPLAY = document.querySelector('.minutes');
-const SECONDSDISPLAY = document.querySelector('.seconds');
+const HOUR_DISPLAY = document.querySelector('.hour');
+const MINUTES_DISPLAY = document.querySelector('.minutes');
+const SECONDS_DISPLAY = document.querySelector('.seconds');
+const INPUT = document.querySelector('.input');
+let VALUE;
 
 function setTimer() {
-    const VALUE = +(document.getElementsByClassName('input')[0].value);
+    VALUE = +(document.getElementsByClassName('input')[0].value);
     let times = VALUE * 60;
+    intervalId2 = setInterval(function run() {
+        times--;
+        function timer() {
+                let time = times;
+            
+                if(time < 0) {
+                    alert('Time is out');
+                    BUTTON.innerHTML = "ta-dah!";
+                    clearInterval(intervalId2);
+                    INPUT.innerHTML = 0;
+                } else {
+                    SECONDS_DISPLAY.innerHTML = dial(time)[0];                  // time = arrTimes from dial()
+                    MINUTES_DISPLAY.innerHTML = dial(time)[1];                 // time = arrTimes from dial()
+                    HOUR_DISPLAY.innerHTML    = dial(time)[2];                // time = arrTimes from dial()
+                    if (dial(time)[0] < 10) {                                // time = arrTimes from dial()
+                        SECONDS_DISPLAY.innerHTML = '0' + dial(time)[0];    // time = arrTimes from dial()
+                    } if (dial(time)[1] < 10) {                            // time = arrTimes from dial()
+                        MINUTES_DISPLAY.innerHTML = '0' + dial(time)[1];  // time = arrTimes from dial()
+                    }
+                }
+            }
+        timer();
+        }, 1000);
     BUTTON.style.background = "red";
     BUTTON.innerHTML = "wait...";
-    intervalId2 = setInterval(function run() {
-        timer(times);
-        times--;
-    }, 1000);
-    
 }
 
-function timer(times) {
-    let time = times;
-
-    if(time < 0) {
-        alert('Time is out');
-        BUTTON.innerHTML = "ta-dah!";
-        clearInterval(intervalId2);
-    } else {
-        SECONDSDISPLAY.innerHTML = dial(time)[0];
-        MINUTESDISPLAY.innerHTML = dial(time)[1];
-        HOURDISPLAY.innerHTML = dial(time)[2];
-        if (dial(time)[0] < 10) {
-            SECONDSDISPLAY.innerHTML = '0' + dial(time)[0];
-        } if (dial(time)[1] < 10) {
-            MINUTESDISPLAY.innerHTML = '0' + dial(time)[1];
-        }
-    }
-}
 
 function dial(time) {
     let arrTimes = [];
