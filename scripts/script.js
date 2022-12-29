9af4a74f7442795a49785b99fda27a81f9063e94
EVG2055 <enivanoff55@mail.ru>
2022-12-29 22:45:55 +0600

    rewrite ver

--- scripts/script.js
+++ scripts/script.js
@@ -1,94 +1,41 @@
-// // 'use strict'; не могу побороть 10 стр.
-// const BUTTON = document.querySelector('.button');
-
-// function setTimer() {
-//     const VALUE = +(document.getElementsByClassName('input')[0].value);
-//     let times = VALUE * 60;
-//     BUTTON.style.background = "red";
-//     BUTTON.innerHTML = "wait...";
-//     intervalId2 = setInterval(function run() {
-//         timer(times);
-//         times--;
-//     }, 1000);
-    
-// }
-
-// function timer(times) {
-//     let time = times;
-//     const seconds = Math.trunc(time % 60); 
-//     const minutes = Math.trunc(time / 60 % 60);
-//     const hour = Math.trunc(time / 60 / 60 % 60);
-//     const HOURDISPLAY = document.querySelector('.hour');
-//     const MINUTESDISPLAY = document.querySelector('.minutes');
-//     const SECONDSDISPLAY = document.querySelector('.seconds');
-
-//     if(time < 0) {
-//         alert('Time is out');
-//         BUTTON.innerHTML = "ta-dah!";
-//         clearInterval(intervalId2);
-//     } else {
-//         SECONDSDISPLAY.innerHTML = seconds;
-//         MINUTESDISPLAY.innerHTML = minutes;
-//         HOURDISPLAY.innerHTML = hour;
-//         if (seconds < 10) {
-//             SECONDSDISPLAY.innerHTML = '0' + seconds;
-//         } if (minutes < 10) {
-//             MINUTESDISPLAY.innerHTML = '0' + minutes;
-//         }
-//     }
-// }
-
-// BUTTON.addEventListener ("click" , setTimer);
-
-
 'use strict';
-const BUTTON = document.querySelector('.button');
-let intervalId2;
-const HOUR_DISPLAY = document.querySelector('.hour');
-const MINUTES_DISPLAY = document.querySelector('.minutes');
-const SECONDS_DISPLAY = document.querySelector('.seconds');
-const INPUT = document.querySelector('.input');
-let VALUE;
 
-function setTimer() {
-    VALUE = +(document.getElementsByClassName('input')[0].value);
-    let times = VALUE * 60;
-    intervalId2 = setInterval(function run() {
-        times--;
-        function timer() {
-                let time = times;
-            
-                if(time < 0) {
-                    alert('Time is out');
-                    BUTTON.innerHTML = "ta-dah!";
-                    clearInterval(intervalId2);
-                    INPUT.innerHTML = 0;
-                } else {
-                    SECONDS_DISPLAY.innerHTML = dial(time)[0];                  // time = arrTimes from dial()
-                    MINUTES_DISPLAY.innerHTML = dial(time)[1];                 // time = arrTimes from dial()
-                    HOUR_DISPLAY.innerHTML    = dial(time)[2];                // time = arrTimes from dial()
-                    if (dial(time)[0] < 10) {                                // time = arrTimes from dial()
-                        SECONDS_DISPLAY.innerHTML = '0' + dial(time)[0];    // time = arrTimes from dial()
-                    } if (dial(time)[1] < 10) {                            // time = arrTimes from dial()
-                        MINUTES_DISPLAY.innerHTML = '0' + dial(time)[1];  // time = arrTimes from dial()
-                    }
-                }
-            }
-        timer();
-        }, 1000);
-    BUTTON.style.background = "red";
-    BUTTON.innerHTML = "wait...";
-}
+const BUTTON = document.getElementById('button');
+const DISPLAY = document.getElementById('display');
+const INPUT = document.getElementById('input');
 
+BUTTON.addEventListener('click', run);
+document.addEventListener('change',  run);
 
-function dial(time) {
-    let arrTimes = [];
+function run() {
+    BUTTON.removeEventListener('click', run);
+    timer();
+} 
 
-    arrTimes.push(Math.trunc(time % 60), Math.trunc(time / 60 % 60), Math.trunc(time / 60 / 60 % 60)); 
+function timer() {
+    var time = INPUT.value * 60;
 
-    return arrTimes;
-}
+    var timeObj = {
+        };
 
-BUTTON.addEventListener ("click" , setTimer);
+    const timerId = setInterval(() => {
+        time--;
+        timeObj.hour = Math.trunc(time / 60 / 60 % 60);
+        timeObj.minute = Math.trunc(time / 60 % 60);
+        timeObj.second =  Math.trunc(time % 60) ;
 
+        for(var elem in timeObj) {
+            var ELEMENT = timeObj[elem];
 
+            if(ELEMENT < 10) {timeObj[elem] = '0' + ELEMENT} ;
+        };
+
+        DISPLAY.innerText = `${timeObj.hour} : ${timeObj.minute} : ${timeObj.second}`;
+       
+        if(time <= 0) {
+            clearInterval(timerId);
+            alert('done');
+            BUTTON.addEventListener('click', run);
+        };
+    } , 1000);
+};
